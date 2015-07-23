efb3baeb7713171b2dbe3e1227dcceac2386a15e
wolfieorama <wachirawamunyi@gmail.com>
2015-07-23 09:46:43 +0300

    added button to navigate to new activity

--- app/src/main/java/com/jjh/drinksposta/MainActivity.java
+++ app/src/main/java/com/jjh/drinksposta/MainActivity.java
@@ -1,16 +1,32 @@
 package com.jjh.drinksposta;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, one.class);
+                startActivity(i);
+            }
+        });
     }
 }
