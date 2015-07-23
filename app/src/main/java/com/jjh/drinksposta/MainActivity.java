073c9a2a7f0bc23e75f3fa7941ebae1fb905e112
wolfieorama <wachirawamunyi@gmail.com>
2015-07-23 14:46:29 +0300

    added a functional list view to the app

--- app/src/main/java/com/jjh/drinksposta/MainActivity.java
+++ app/src/main/java/com/jjh/drinksposta/MainActivity.java
@@ -3,28 +3,34 @@ package com.jjh.drinksposta;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    Button button2;
+    Button button;
+    TextView textview3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, one.class);
                 startActivity(i);
             }
         });
+
+        TextView textview3 = (TextView) findViewById(R.id.textView3);
+        textview3.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
---
+++ app/src/main/java/com/jjh/drinksposta/story1.java
@@ -0,0 +1,14 @@
+package com.jjh.drinksposta;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class story1 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.story1);
+    }
+}
+
